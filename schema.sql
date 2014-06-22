f19ab3d8f7b5c0ac81a6c28494c04e443ae9bada
Greg Heartsfield <scsibug@imap.cc>
2014-06-21 19:49:44 -0500

    Added schema describing monitoring points, values, and events.

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Store details about monitoring points
+create table monitor_points (
+  id serial primary key,
+  name text unique not null,
+  units text, -- units for the recorded data points.  May be null for unitless event.
+  description text
+);
+
+-- Store values recorded at monitoring points
+create table point_values (
+  id serial primary key,
+  monitor_point serial references monitor_points(id),
+  numeric_val numeric,
+  tstamp timestamp with time zone
+);
+
+-- Store events recorded at monitoring points
+create table point_events (
+  id serial primary key,
+  monitor_point serial references monitor_points(id),
+  event text,
+  tstamp timestamp with time zone
+);
