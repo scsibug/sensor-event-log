5d37c378ab3c44af6d32e0266233b236044a2947
Greg Heartsfield <scsibug@imap.cc>
2014-08-05 19:32:28 -0500

    Update schema to include agent/sensor/sample tables.

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
 create extension if not exists hstore;
+
 -- Agents authenticate and publish data to the collector
 create table agents (
   id serial primary key,
   name text unique not null,
   signing_key text,
   description text,
-  created timestamp with time zone
+  created timestamp with time zone,
+  attr hstore -- agent metadata
 );
 
 -- Sensors interact with the environment to read values/detect change
@@ -16,7 +18,8 @@ create table sensors (
   units text, -- "count", "celsius", "seconds", "event"; some indication of what a reading means
   description text, -- human readable description of the sensor
   coalesce boolean, -- should we attempt to coalesce identical readings into the same row?
-  created timestamp with time zone -- when 
+  created timestamp with time zone, -- when the reading occurred
+  attr hstore -- sensor metadata
 );
 
 -- Store numeric or discrete (event) samples from a sensor
@@ -27,12 +30,7 @@ create table samples (
   numeric_val numeric, -- sample from a numeric sensor
   event text, -- event or state from a discrete sensor
   last_seen timestamp with time zone, -- for coalesced sensors, when was the last time we saw this value?
-  times_seen integer -- how many readings have been sent with this same value?
+  times_seen integer, -- how many readings have been sent with this same value?
+  attr hstore -- sample metadata
 );
 
--- a reading could include key/value pairs, recorded here
-create table sample_meta (
-  id bigserial primary key,
-  sample bigserial references samples(id),
-  attr hstore
-);
