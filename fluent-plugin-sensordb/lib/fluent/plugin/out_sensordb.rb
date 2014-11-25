8bbf1cb6fb970016f8618d84e93126647cb3c593
Greg Heartsfield <scsibug@imap.cc>
2014-11-24 22:56:14 -0600

    Misc updates to fluent logging.

--- fluent-plugin-sensordb/lib/fluent/plugin/out_sensordb.rb
+++ fluent-plugin-sensordb/lib/fluent/plugin/out_sensordb.rb
@@ -10,7 +10,7 @@ class Fluent::SensorDBOutput < Fluent::BufferedOutput
 
   def initialize
     super
-    log.info "initializing sensordb"
+    log.info "initializing sensordb 0.0.1"
     require 'pg'
     require 'time'
   end
@@ -41,6 +41,20 @@ class Fluent::SensorDBOutput < Fluent::BufferedOutput
       chunk.msgpack_each {|(tag, time_str, record)|
         begin
           r=r+1
+          # Determine what type of message this is.
+          mtype = record["msg_type"]
+          if (mtype == nil or mtype == "sample")
+            log.info "SAMPLE message"
+            record_sample(conn, time_str, record)
+          elsif (mtype == "sensor_metadata")
+            log.info "SENSOR METADATA message"
+            record_metadata(conn, time_str, record)
+          elsif (mtype == "heartbeat")
+            log.info "HEARTBEAT message"
+            record_heartbeat(conn, time_str, record)
+          else
+            log.error "Unknown message type #{mtype}"
+          end
           # time is a unix integer, convert this into 8601 format
           isotime = Time.at(time_str).iso8601
           conn.exec_params('INSERT INTO supersimple (created, reading) VALUES ($1, $2)', [isotime, record["value"]])
@@ -55,6 +69,19 @@ class Fluent::SensorDBOutput < Fluent::BufferedOutput
     log.info "Time elapsed #{(end_time - beginning_time)*1000}ms"
     log.info "  Wrote #{r} records, #{elapsed_ms/r}ms per record"
     log.info "  This is #{(r/elapsed_ms)*1000} rows/sec"
+  end
+
+  def record_sample(conn, time_str, record)
+    log.info "sample metadata received, ignoring for now"
+    log.info "time was #{time_str}"
+  end
+
+  def record_metadata(conn, time_str, record)
+    log.info "sensor metadata received, ignoring for now"
+  end
+
+  def record_heartbeat(conn, time_str, record)
+    log.info "heartbeat received, ignoring for now"
   end
 
   def get_connection()
--- simple_fluent_sender.py
+++ simple_fluent_sender.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from fluent import sender
 
 # for local fluent
@@ -7,8 +8,9 @@ from fluent import event
 import time
 while True:
     # Send a message every 500us 
-    time.sleep(0.3 / 1000.0)
+    time.sleep(1.0)
     # send event to fluentd, with 'app.follow' tag
     event.Event('test', {
             'value': '23423'
             })
+    print "send message"
